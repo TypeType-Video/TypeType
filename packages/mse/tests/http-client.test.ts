351e44be40612d9127cdd886b1e256ac63e6eb1a
Priveetee <brahim.benyoucefpro@proton.me>
2026-07-10 15:27:34 +0200

    chore: consume public mse package

--- packages/mse/tests/http-client.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "bun:test";
-import { HttpClient } from "../src/http-client";
-
-test("preserves endpoint path for absolute API URLs", () => {
-  const http = new HttpClient({ endpoint: "https://beta.typetype.video/api" });
-  expect(http.absolute("/sabr/playback/session/manifest")).toBe(
-    "https://beta.typetype.video/api/sabr/playback/session/manifest",
-  );
-});
